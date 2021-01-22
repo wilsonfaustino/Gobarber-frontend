f794f7f02bcf8e84f53bbdb4a9f3e727f9dc27fe
Wilson Faustino <wilson.rfaustino@gmail.com>
2021-01-21 23:14:15 -0300

    Isolating Input and Button component, SignUp Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import SigIn from './pages/SignIn';
+// import SigIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SigIn />
+    {/* <SigIn /> */}
+    <SignUp />
     <GlobalStyle />
   </>
 );
